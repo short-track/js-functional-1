dda82a55dcb177dcb42931e7fbb298fdc936cb78
jeyoon <wpdbs4419@gmail.com>
2023-07-24 20:58:05 +0900

    [2주차] 폴더 셋팅 및 섹셕 4,5 복습

--- jeyoon/section4/fx.js
+++ jeyoon/section4/fx.js
@@ -4,7 +4,9 @@ const curry =
   (f) =>
   (a, ..._) =>
     _.length ? f(a, ..._) : (..._) => f(a, ..._);
+    // 인자가 두개 이상이라면 함수 즉시 실행, 두개보다 작으면 다 합쳐서 실행 
 
+    // ** map, filter, reduce에 curry 적용: 인자를 하나만 받았을 경우, 일단 이후 인자를 받기 위힌 힘수를 리턴
 const map = curry((f, iter) => {
   let res = [];
   for (const a of iter) {
