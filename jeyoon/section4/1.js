421a110a833f489495787c9a919f93fcfc9e51b2
Jeyoon <wpdbs4419@gmail.com>
2023-07-30 01:56:30 +0900

    [2주차] 섹션 6 정리

---
+++ jeyoon/section4/1.js
@@ -0,0 +1,16 @@
+const log = console.log;
+
+const curry =
+  (f) =>
+  (a, ..._) =>
+    _.length ? f(a, ..._) : (..._) => f(a, ..._);
+
+const map = curry((f, iter) => {
+  let res = [];
+  for (const a of iter) {
+    res.push(f(a));
+  }
+  return res;
+});
+
+const filter = curry((f, iter) => {});
